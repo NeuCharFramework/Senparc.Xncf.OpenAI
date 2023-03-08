9286435cf5d38b17a67539d542f7805cc86d4784
Rodear <szhang@senparc.com>
2023-03-08 18:03:54 +0800

    配置打包文件路径，名称

--- src/front-end/Senparc.Xncf.OpenAI/src/module.js
+++ src/front-end/Senparc.Xncf.OpenAI/src/module.js
@@ -4,12 +4,12 @@ import storeModule from './store/store-module'
 export default function (Vue) {
   Vue.config.productionTip = true
   // console.info('编译完成');
-  // this.$router.addRoutes(routes)
   routes.forEach(ele => {
     console.log('ele', ele);
     this.$router.addRoute('Module', ele)
   })
   console.info('routes', routes)
+
   this.$store.registerModule('main', storeModule)
   this.$eventBus.on('visitedAbout', (e) => {
     console.info(e);
--- src/front-end/Senparc.Xncf.OpenAI/src/store/store-module.js
+++ src/front-end/Senparc.Xncf.OpenAI/src/store/store-module.js
@@ -1,7 +1,7 @@
 export default {
   namespaced: true,
   state: {
-    title: 'ncf-module'
+    title: 'SenparcXncfOpenAI'
   },
   mutations: {}
 }
