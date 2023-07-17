10e3406c3ea42a8fba33ef44bb4bd3834f3407cb
汤问 <tonwin618@qq.com>
2023-07-17 19:03:55 +0800

    删除CreateCompletionAsync

--- src/back-end/Senparc.Xncf.OpenAI/OHS/Local/AppService/GPT3AppService.cs
+++ src/back-end/Senparc.Xncf.OpenAI/OHS/Local/AppService/GPT3AppService.cs
@@ -16,57 +16,6 @@ namespace Senparc.Xncf.OpenAI.OHS.Local.AppService
         {
             _openAiService = openAiService;
         }
-        /**
-        /// <summary>
-        /// 使用不同模型运行 OpenAI
-        /// </summary>
-        /// <param name="prompt">prompt 提示信息</param>
-        /// <param name="model">选用模型，如果留空则默认使用 text-davinci-v3</param>
-        /// <param name="maxTokens">最大消费 Token 数量。默认为 50</param>
-        /// <returns></returns>
-        /// <exception cref="NcfExceptionBase"></exception>
-        [ApiBind(ApiRequestMethod = CO2NET.WebApi.ApiRequestMethod.Post)]
-        public async Task<AppResponseBase<List<ChoiceResponse>>> CreateCompletionAsync(string prompt, string model = null, int maxTokens = 50)
-        {
-            return await this.GetResponseAsync<AppResponseBase<List<ChoiceResponse>>, List<ChoiceResponse>>(async (response, logger) =>
-            {
-                var dt1 = SystemTime.Now;//开始计时
-
-                var openAIService = await _openAiService.GetOpenAiServiceAsync();
-
-                if (model.IsNullOrEmpty())
-                {
-                    model = global::OpenAI.GPT3.ObjectModels.Models.CodeDavinciV2;
-                }
-
-                //openAIService.SetDefaultModelId(global::OpenAI.GPT3.ObjectModels.Models.TextDavinciV3);
-                var completionResult = await openAIService.Completions.CreateCompletion(new CompletionCreateRequest()
-                {
-                    Prompt = prompt,
-                    Model = model,
-                    MaxTokens = maxTokens,
-
-                });
-
-                if (completionResult.Successful)
-                {
-                    return completionResult.Choices;//.FirstOrDefault();
-                }
-                else //handle errors
-                {
-                    if (completionResult.Error == null)
-                    {
-                        throw new NcfExceptionBase("Unknown Error");
-                    }
-                    else
-                    {
-                        throw new NcfExceptionBase($"Error：{completionResult.Error.Message}");
-                    }
-                }
-
-            });
-        }
-        **/
 
         /// <summary>
         /// 使用不同模型运行 OpenAI（使用 Stream 方式）
